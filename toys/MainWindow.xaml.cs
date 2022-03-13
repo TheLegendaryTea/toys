d4204ea382b8c93b2ce45f8076618adc5af548b2
PC <PC@DESKTOP-VH0UJ4V>
2022-03-13 23:26:26 +0200

    Добавьте файлы проекта.

---
+++ toys/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace toys
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public enum Type
+    {
+        Снеговик=1,Машинка=2,Звезда=4,Снежинка=8,Петля=16,Мыло=32,Стул=64
+    }
+    public partial class MainWindow : Window
+    {
+        private Toys toys;
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (string str in Enum.GetNames(typeof(Type)))
+            {
+                CheckBox checkBox = new CheckBox
+                {
+                    Content = str,
+                    MaxHeight = 30,
+                    IsChecked = false,
+                    Margin = new Thickness(10)
+                };
+                checkBox.Checked += checkBox_checked;
+                Patterns.Children.Add(checkBox);
+            }
+        }
+        private void checkBox_checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            Type properties = (Type)Enum.Parse(typeof(Type),
+                                        checkBox.Content.ToString());
+            toys.Pattern = properties;
+            Result.Text = "";
+            foreach (string str in toys.NaborHavePet())
+            {
+                Result.Text += str + "\n";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+            int n = Convert.ToInt32(nBox.Text);
+            toys = new Toys(n);
+            toys.FormNabor();
+            Type[] nabor = toys.GetNabor();
+            for (int i = 0; i < Patterns.Children.Count; i++)
+            {
+                (Patterns.Children[i] as CheckBox).IsChecked = false;
+            }
+            string[] strNabor = toys.GetStrNabor();
+            for (int i = 0; i < n; i++)
+            {
+                Result.Text += "Набор " + (i + 1) + "  \n";
+                Result.Text += strNabor[i] + '\n';
+            }
+        }
+
+        private void Result_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ toys/Toys.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toys
+{
+    public class Toys
+    {
+        private int n;
+        private Random random;
+        private Type[] nabor;
+        private Type pattern;
+        private string[] strNabor;
+
+        public Toys()
+        {
+            n = 10;
+            nabor = new Type[n];
+            strNabor = new string[n];
+            random = new Random();
+        }
+        public Toys(int n)
+        {
+            this.n = n;
+            nabor = new Type[n];
+            strNabor = new string[n];
+            random = new Random();
+        }
+        public Toys(Type[] pp)
+        {
+            this.n = pp.Length;
+            nabor = new Type[n];
+            strNabor = new string[n];
+            random = new Random();
+        }
+        public Type Pattern
+        {
+            set { pattern = value; }
+        }
+        public void FormNabor()
+        {
+            int properties = 7;
+            int p = 0, q = 0, currentProps = 0;
+            string strQ;
+            for (int i = 0; i < n; i++)
+            {
+                currentProps = 0; strQ = "";
+                for (int j = 0; j < properties; j++)
+                {
+                    p = random.Next(2);
+                    q = (int)Math.Pow(2, j);
+                    if (p == 1)
+                    {
+                        currentProps += q;
+                        strQ += (Type)q + ", ";
+                    }
+                }
+                nabor[i] = (Type)currentProps;
+                if (strQ != "") strNabor[i] = strQ.Remove(strQ.Length - 2);
+                else strNabor[i] = "";
+            }
+        }
+        public string[] GetStrNabor()
+        {
+            return strNabor;
+        }
+        public Type[] GetNabor()
+        {
+            return nabor;
+        }
+
+        public string[] NaborHavePet()
+        {
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if((nabor[i]&pattern)== pattern)
+                {
+                    k++;
+                }
+            }
+            string[] ret = new string[k];
+            k= 0;
+            for(int i = 0; i < n; i++)
+            {
+                if((nabor[i]&pattern)== pattern)
+                {
+                    ret[k]="Набор["+(i+1)+"]";
+                    k++;
+                }
+            }
+            return ret;
+        }
+
+        
+    }
+
+    
+}
